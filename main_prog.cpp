b875fa1980a154630a83391f57beebd79503ab47
Rohit Sant <rhsant@gmail.com>
2015-11-24 22:42:47 -0500

    Revert "Code breaks in odom step after laser"
    This reverts commit 758a044045f86785cf60c5322047d8df850d1228.

--- main_prog.cpp
+++ main_prog.cpp
@@ -6,34 +6,26 @@
 
 #define PI 3.1415926
 
-int num_particles = 5120;
+int num_particles = 1024;
 float laser_variance = 0.1;
 
-#pragma warning( push )
-#pragma warning (disable:4101)
 
 int main()
 {
-	//Preprocess data
-
-	//Read map
 	char* mapName = "map/wean.dat";
 	map* map_obj = new map;
 	map_obj->map_read(mapName);
 	//map_obj->map_print();
-	std::cout<<"Map Read."<<std::endl;
-	//Read sensor data
 	SensorData* laser_obj = new SensorData;
 	char* laserLog = "log/robotdata1.log";
 	laser_obj->read_data(laserLog);
 	//laser_obj->print_data();
-	std::cout<<"Sensor Data got."<<std::endl;
-	//Initialize particles
+
+
 	int x_delta = (int)map_obj->get_size_x()/sqrt(num_particles/5);
 	int y_delta = (int)map_obj->get_size_y()/sqrt(num_particles/5);
-	std::cout<<"Sizes got."<<std::endl;
 	float theta_delta[5] = {0, PI/4, PI/2, 3*PI/4, PI};
-	std::vector<particle*>* particles = new std::vector<particle*>;
+	std::vector<particle*>* particles;
 	
 	for (int x = 0; x< map_obj->get_size_x(); x+=x_delta)
 		for (int y = 0; y< map_obj->get_size_y(); y+=y_delta)
@@ -42,58 +34,11 @@ int main()
 				particle* new_particle = new particle((float)x,(float)y, theta_delta[j]);
 				particles->push_back(new_particle);
 			}
-	std::cout<<"Particles Initialized."<<std::endl;
-	//Preprocess odometry and sensor data
-	std::vector<char>* ordering = laser_obj->get_ordering();
-	std::vector<float>* cur_odom = new std::vector<float>;
-	std::vector<int>* cur_laser;
-	bool no_laser = false;
-	int laser_iter = 0;
-
-	for (int t = 1; t< ordering->size(); t++)
+	for (int t = 0; t< laser_obj->get_laser_scan()->size(); t++)
 	{
-		std::cout << "T = " << t << std::endl;
-		//Calculate the odometry
-		cur_odom->clear();
-		std::cout<<"Cur odom Cleared"<<std::endl;
-		for (int i = 0; i < 3; i++)
-			cur_odom->push_back((*(laser_obj->get_odom_data()))[t][i] - (*(laser_obj->get_odom_data()))[t-1][i]);
-		std::cout<<"New cur odom done"<<std::endl;
-
-		if ((*ordering)[t] == 'O')
-		{
-			no_laser = true;
-
-			//No need to change laser data
-			std::cout<<"Entered Odometry step"<<std::endl;
-			particles = particle_filter(map_obj, particles, cur_odom, cur_laser, &(*(laser_obj->get_laser_relative_pose()))[laser_iter], laser_variance, no_laser, map_obj->get_size_x(), map_obj->get_size_y());
-			no_laser = false;
-			std::cout<<"Odometry step."<<std::endl;
-		}
-		else if((*ordering)[t] == 'L')
-		{
-			laser_iter++;
-			cur_laser = &(*(laser_obj->get_laser_scan()))[laser_iter];
-			particles = particle_filter(map_obj, particles, cur_odom, cur_laser, &(*(laser_obj->get_laser_relative_pose()))[laser_iter], laser_variance, no_laser, map_obj->get_size_x(), map_obj->get_size_y());
-			std::cout<<"Laser step."<<std::endl;
-		}
+	
+		particles = particle_filter(map_obj, particles, laser_obj->get_odom_data(), laser_obj->get_laser_scan(), laser_obj->get_laser_pose(), laser_variance);
 	
 	}
-	for (std::vector<particle*>::iterator iter_particles = particles->begin(); iter_particles != particles->end(); iter_particles++)
-	{
-		std::cout<<"About to free"<<std::endl;
-		delete(*iter_particles);
-		std::cout<<"Freed individual particles."<<std::endl;
-	}
-	
-	particles->clear();
-	std::cout<<"Cleared particles vector."<<std::endl;
-	delete(particles);
-	std::cout<<"Deleted particles vector."<<std::endl;
-	delete(cur_odom);
-	std::cout<<"Deleted odom vector."<<std::endl;
-	
-	return 0;
+
 }
-
-#pragma warning (pop)
--- particle_filter.cpp
+++ particle_filter.cpp
@@ -10,38 +10,31 @@
 
 
 
-float compute_weight(map* map_data, particle* current_particle, std::vector<int>* laser_observation_t, std::vector<float>* laser_relative_pose_t, float laser_variance, int size_x, int size_y)
+float compute_error(map* map_data, particle* current_particle, std::vector<int>* laser_observation_t, std::vector<float>* laser_relative_pose_t, float laser_variance)
 {
+	std::vector<float>* laser_map_frame;
 	int count = 0;
-	float cumul_weight = 0;
-	float weight = 0;
+	float cumul_err = 0;
 	for (std::vector<int>::iterator iter_laser = laser_observation_t->begin(); iter_laser != laser_observation_t->end(); iter_laser++)
 	{
 		
 		int x = (int) (current_particle->get_x() + (*iter_laser)*cos(count*PI/180) + (*laser_relative_pose_t)[0]);
 		int y = (int) (current_particle->get_y() + (*iter_laser)*sin(count*PI/180) + (*laser_relative_pose_t)[1]);
-		
 
-		if (x > 0 && y > 0 && x < size_x && y < size_y)
-			weight = (*(map_data->get_map_grid()))[x][y]/laser_variance; //Remove the 1- if it turns out that the map fill-in is inverted
-		else
-			weight = 0; 
-		
-		//std::cout<<"Weight = "<< weight << std::endl;
-		cumul_weight = cumul_weight + pow(weight,2);
+		float err = (1-(*(map_data->get_map_grid()))[x][y])/laser_variance; //Remove the 1- if it turns out that the map fill-in is inverted
+		cumul_err = cumul_err + pow(err,2);
 		count++;
 	}
-	return cumul_weight;
+	return cumul_err;
 }
 
-std::vector<particle*>* particle_filter(map* map_data, std::vector< particle* >* X_prev, std::vector<float>* odom_t, std::vector<int>* laser_observation_t, std::vector<float>* laser_relative_pose_t, float laser_variance, bool no_laser, int size_x, int size_y)
+std::vector<particle*>* particle_filter(map* map_data, std::vector< particle* >* X_prev, std::vector<float>* odom_t, std::vector<int>* laser_observation_t, std::vector<float>* laser_relative_pose_t, float laser_variance)
 {
-	std::vector< particle* >* temp_particles = new std::vector <particle*>;
+	std::vector< particle* > temp_particles;
 	std::vector<float> weights;
 
 	int num_particles = X_prev->size();
 	
-	int debug_count = 0;
 	//Candidate distribution generation
 	for (std::vector<particle*>::iterator iter_particles = X_prev->begin(); iter_particles != X_prev->end(); iter_particles++)
 	{
@@ -50,48 +43,15 @@ std::vector<particle*>* particle_filter(map* map_data, std::vector< particle* >*
 		new_particle->set_x((*iter_particles)->get_x() + (*odom_t)[0]);
 		new_particle->set_y((*iter_particles)->get_y() + (*odom_t)[1]);
 		new_particle->set_theta((*iter_particles)->get_theta() + (*odom_t)[2]);
-		temp_particles->push_back(new_particle);
-		
-		debug_count++;
-		std::cout<<"New particle count "<< debug_count << std::endl;
-
+		temp_particles.push_back(new_particle);
 	}
 
-	//Check if inertial preintegration case
-	if (no_laser)
-	{
-		//Clear old particles
-		int debug_count = 0;
-		for (std::vector<particle*>::iterator iter_particles = X_prev->begin(); iter_particles != X_prev->end(); iter_particles++)
-		{
-			
-			std::cout<<"No error here pls 1: "<<debug_count << std::endl;
-			debug_count++;
-			delete(*iter_particles);
-		}
-		std::cout<<"No error here pls 2"<<std::endl;
-		X_prev->clear();
-		
-		//Set new particles as temp particles
-		for (int i=0; i<num_particles; i++)
-		{
-			X_prev->push_back((*temp_particles)[i]);
-		}
-		std::cout<<"No error here pls 3"<<std::endl;
-		temp_particles->clear();
-		delete(temp_particles);
-		return X_prev;
-	}
-	//Else there is a laser reading
 	//Calculate weights
 	weights.clear();
-	
-	std::cout<<"Weights cleared."<<std::endl;
-	
 	for (std::vector<particle*>::iterator iter_particles = X_prev->begin(); iter_particles != X_prev->end(); iter_particles++)
 	{
-		weights.push_back(compute_weight(map_data,*iter_particles, laser_observation_t, laser_relative_pose_t, laser_variance, size_x, size_y));
-		//std::cout<<"Weight pushed back."<<std::endl;
+		weights.push_back(compute_error(map_data,*iter_particles, laser_observation_t, laser_relative_pose_t, laser_variance));
+		
 	}
 
 	//Resample
@@ -108,17 +68,14 @@ std::vector<particle*>* particle_filter(map* map_data, std::vector< particle* >*
 	X_prev->clear();
 
 	//Fill up the new particles in X_prev
-	int random_pick = 0;
 	for (int i=0; i<num_particles; i++)
 	{
-		random_pick = (*weight_dist)(generator);
-		std::cout << "Random choice = " << random_pick << std::endl;
-		X_prev->push_back((*temp_particles)[random_pick]);
+		
+		X_prev->push_back(temp_particles[(*weight_dist)(generator)]);
 	}
-	temp_particles->clear();
-	delete(temp_particles);
+	temp_particles.clear();
 	delete(weight_dist);
-	std::cout << "Size = " << X_prev->size() << std::endl;
+
 	return X_prev;
 
 }
